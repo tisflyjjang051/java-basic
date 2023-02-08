201890f0103c0a5c7e30825789371cfe1314f007
jang sung ho <tisflyjjang051@gmail.com>
2023-02-08 15:34:43 +0900

    ch05 practice10

--- ch05/src/practice/Practice10.java
+++ ch05/src/practice/Practice10.java
@@ -16,27 +16,72 @@ abstract class PairMap {
 
 class Dictionary extends PairMap {
 
-  String get(String key) {
-    return "aa";
+  int count = 0;
+  int i = 0;
+
+  public Dictionary(int capacity) {
+    keyArray = new String[capacity];
+    valueArray = new String[capacity];
+    // keyArray["a","b","c"];
+    // valueArray["apple","boy","circle"];
   }
 
-  void put(String key, String value) {}
+  String get(String key) {
+    for (int i = 0; i < count; i++) {
+      if (keyArray[i].equals(key)) {
+        return valueArray[i];
+      }
+    }
+    return null;
+  }
+
+  void put(String key, String value) {
+    int i = 0;
+    for (i = 0; i < count; i++) {
+      if (keyArray[i].equals(key)) {
+        break;
+      }
+    }
+    System.out.println("i====" + i);
+
+    if (i == count) { // 키가 발견이 안됐음....
+      keyArray[i] = key;
+      valueArray[i] = value;
+      count++;
+    } else {
+      keyArray[i] = key;
+      valueArray[i] = value;
+    }
+    // 키가 중복되면 value를 대치....
+  }
 
   String delete(String key) {
-    return "";
+    String result = null;
+    for (int i = 0; i < count; i++) {
+      if (keyArray[i].equals(key)) {
+        keyArray[i] = null;
+        valueArray[i] = null;
+        result = valueArray[i];
+      }
+    }
+    count--;
+    return result;
   }
 
   int length() {
-    return 0;
+    return count;
   }
 }
 
 public class Practice10 {
 
   public static void main(String[] args) {
-    Dictionary dictionary = new Dictionary();
-    dictionary.put("aa", "bb");
-    System.out.println(dictionary.get("aa"));
-    dictionary.delete("aa");
+    Dictionary dictionary = new Dictionary(10);
+    dictionary.put("황기태", "자바");
+    dictionary.put("이재문", "파이썬");
+    dictionary.put("이재문", "C++");
+    System.out.println("황기태의 값은 " + dictionary.get("황기태"));
+    System.out.println("이재문의 값은 " + dictionary.get("이재문"));
+    System.out.println(dictionary.delete("황기태"));
   }
 }
---
+++ ch05/src/practice/Practice11.java
@@ -0,0 +1,3 @@
+package practice;
+
+public class Practice11 {}
