cfa8dda6c07980c4e37eec26d53b187dd44b4e96
jang sung ho <tisflyjjang051@gmail.com>
2023-02-13 14:31:54 +0900

    file

---
+++ ch08/src/FileInputSteamEx.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * FileInputSteamEx
+ */
+public class FileInputSteamEx {
+
+  public static void main(String[] args) {
+    byte b[] = new byte[6];
+    try {
+      FileInputStream fileInput = new FileInputStream("c:\\Temp\\test.out");
+      int n = 0;
+      int c = 0;
+      while ((c = fileInput.read()) != -1) {
+        b[n] = (byte) c;
+        n++;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
