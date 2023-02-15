66606c6cf91ad9730771a617f3be3f6760f015cc
jang sung ho <tisflyjjang051@gmail.com>
2023-02-15 09:44:59 +0900

    ch10 MOuseListener

--- ch10/src/MouseListenerEx.java
+++ ch10/src/MouseListenerEx.java
@@ -1,22 +1,48 @@
 import java.awt.Container;
+import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+/**
+ * MouseListenerEx
+ */
 public class MouseListenerEx extends JFrame {
 
-  private JLabel label = new JLabel("hello");
+  private JLabel label = new JLabel("Hello");
 
   public MouseListenerEx() {
-    this.setTitle("Mouse Event 예제");
+    this.setTitle("Action Event");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     Container contentPane = this.getContentPane();
+    contentPane.addMouseListener(new MyMouseListener());
     contentPane.setLayout(null);
     label.setLocation(100, 100);
     label.setSize(50, 20);
     contentPane.add(label);
-    contentPane.addMouseListener(null);
     this.setSize(300, 300);
     this.setVisible(true);
+  }
+
+  class MyMouseListener implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+      int x = e.getX();
+      int y = e.getY();
+      label.setLocation(x, y);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
   }
 
   public static void main(String[] args) {
