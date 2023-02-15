600e18e03ac7b2fa91b1f35b44c0641b8fc72797
jang sung ho <tisflyjjang051@gmail.com>
2023-02-15 15:55:24 +0900

    ch10 end

--- ch10/src/practice/Practice05.java
+++ ch10/src/practice/Practice05.java
@@ -13,15 +13,22 @@ public class Practice05 extends JFrame {
     Container contentPane = this.getContentPane();
     contentPane.setLayout(new FlowLayout());
     JLabel label = new JLabel("I LOVE JAVA");
+    label.setFont(new Font("Arial", Font.BOLD, 16));
     label.addKeyListener(
       new KeyAdapter() {
         public void keyPressed(KeyEvent e) {
-          if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+          if (e.getKeyCode() == KeyEvent.VK_ADD || e.getKeyChar() == '+') {
             JLabel label = (JLabel) e.getSource();
-            String txt = label.getText();
-            String firstTxt = txt.substring(0, 1); // 첫번쨰 글자
-            String restTxt = txt.substring(1); // 첫번쨰를 제외한 나머지 글자
-            label.setText(restTxt.concat(firstTxt));
+            Font font = label.getFont();
+            int size = font.getSize();
+            label.setFont(new Font("Arial", Font.BOLD, size + 5));
+          } else if (
+            e.getKeyCode() == KeyEvent.VK_SUBTRACT || e.getKeyChar() == '-'
+          ) {
+            JLabel label = (JLabel) e.getSource();
+            Font font = label.getFont();
+            int size = font.getSize();
+            label.setFont(new Font("Arial", Font.BOLD, size - 5));
           }
         }
       }
