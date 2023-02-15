929190ec515cb44e81dc11b764add17e83e97077
jang sung ho <tisflyjjang051@gmail.com>
2023-02-15 13:45:51 +0900

    open challenge end

--- ch10/src/openChallenge/Game.java
+++ ch10/src/openChallenge/Game.java
@@ -3,6 +3,7 @@ package openChallenge;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -15,6 +16,35 @@ public class Game extends JFrame {
     this.setTitle("카지노");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     Container contentPane = this.getContentPane();
+    contentPane.addKeyListener(
+      new KeyAdapter() {
+        public void keyPressed(KeyEvent e) {
+          if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            // System.out.println("enter");
+            // random하게 값을 뽑아서 label에 표시하기...
+            int num01 = (int) (Math.random() * 5);
+            int num02 = (int) (Math.random() * 5);
+            int num03 = (int) (Math.random() * 5);
+            label[0].setText("" + num01);
+            label[1].setText("" + num02);
+            label[2].setText("" + num03);
+            if (num01 == num02 && num02 == num03) {
+              resultLabel.setText("축하합니다.");
+            } else {
+              resultLabel.setText("아쉽습니다.");
+            }
+          }
+        }
+      }
+    );
+    contentPane.addMouseListener(
+      new MouseAdapter() {
+        public void mouseClicked(MouseEvent e) {
+          Container contentPane = (Container) e.getSource();
+          contentPane.requestFocus();
+        }
+      }
+    );
     contentPane.setLayout(null);
     for (int i = 0; i < 3; i++) {
       label[i] = new JLabel("0");
@@ -34,6 +64,8 @@ public class Game extends JFrame {
     contentPane.add(resultLabel);
     this.setSize(500, 300);
     this.setVisible(true);
+    this.setFocusable(true);
+    this.requestFocus();
   }
 
   public static void main(String[] args) {
